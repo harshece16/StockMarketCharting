cd4b88115d3bcb234e0ff2d75329aaa4fa1ee909
harshece16 <harsh.gupta.ece16@itbhu.ac.in>
2020-08-30 15:51:20 +0530

    Delete AdminService.java

--- User-Admin-Service/src/main/java/com/hg/fsd/userservice/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.hg.fsd.userservice.service;
-import com.hg.fsd.userservice.entity.User;
-
-public interface AdminService  {
-	
-	public User findUser(String user_name, String password);
-
-}
